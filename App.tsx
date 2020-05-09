0c13f736cefb17bc7d77dab5a1cc76092975cc26
Pradeep Chakravarti <pradeepchakravarti@Pradeeps-MacBook-Pro.local>
2020-05-08 22:20:41 -0700

    created basic folders

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Nutrition Facts</Text>
+      <Image source = {{}}></Image>
     </View>
   );
 }
