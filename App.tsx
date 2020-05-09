d813d69e4e2ae3ab540f3047da275a8fd6744ba9
Pradeep Chakravarti <pradeepchakravarti@Pradeeps-MacBook-Pro.local>
2020-05-09 15:28:03 -0700

    build freezing at 100%

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
+
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Nutrition Facts</Text>
-      <Image source = {{}}></Image>
+  
     </View>
   );
 }
