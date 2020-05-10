2c0122adfd20e502e1781dfc0c6f9d451aecd3a7
Pradeep Chakravarti <pradeepchakravarti@Pradeeps-MacBook-Pro.local>
2020-05-09 19:51:49 -0700

    hello world

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-
-
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Nutrition Facts</Text>
-  
+      <Text> Hello its me</Text>
     </View>
   );
 }
