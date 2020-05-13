ba7161368aee2312107e63aa6bd2088e55bc37b6
Pradeep Chakravarti <pradeepchakravarti@Pradeeps-MacBook-Pro.local>
2020-05-13 10:26:02 -0700

    logo working in header

--- App.tsx
+++ App.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import Header from './src/library/components/header/Header';
+import {Header} from 'react-native-elements';
 
 export default function App() {
   return(
-    <View>
-      <Header />
+    <View style={styles.container}>
+      <Header
+        barStyle="light-content"
+        leftComponent={{ icon: 'menu', color: '#000' }}
+        centerComponent={<CenterLogo /> }
+        rightComponent={{ icon: 'home', color: '#000' }}
+        containerStyle={{
+          height: 100,
+          backgroundColor: '#ffddab',
+          justifyContent: 'space-around',
+        }}
+      />
       <View style={styles.container}>
         
         <Text>Nutrition Facts</Text>
         <Text> Hello its me</Text>
+        <Text> yo soy health</Text>
+        
       </View>
 
     </View>
   );
+}
+
+export class CenterLogo extends React.Component {
+  render() {
+    return <Image style={this.styles.img} source={require('./intakegreencopy.png')} />;
+  }
+
+  styles = StyleSheet.create({
+    
+    img: {
+      height: 45,
+      width: 45 * 3.174,
+    },
+  });
 }
 
 const styles = StyleSheet.create({
@@ -22,9 +48,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  img: {
-    height: 100,
-    width: 100,
   }
 });
